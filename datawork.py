602da6ee1fc30804a9e819a4a3f9efc1119b7ef4
kappaprideonly <kolmanoney@gmail.com>
2022-04-05 01:53:55 +0300

    classic

--- datawork.py
+++ datawork.py
@@ -1,7 +1,7 @@
 import random
 
 def get_variant(task_number, data):
-    variant = random.randint(0, len(data[task_number]))
+    variant = random.randint(0, len(data[task_number]) - 1)
     answer = data[task_number][variant][1]
     answer = answer[answer.index(" ") + 1:]
     return data[task_number][variant][0], answer
